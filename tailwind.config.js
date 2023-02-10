fa7ec2f724af7305e0a04e51de7b57bf3f8cf720
bora-sen <bahattinborasen@gmail.com>
2023-02-10 13:06:14 +0300

    feat: added working directory and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,jsx}"],
+  content: [
+    "./src/**/*.{html,js,jsx}",
+    "./public/**/*.{html,js,jsx}"
+],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'primary-dark':'#21231b',
+        'primary-light':'#fdfefd',
+        'primary-accent':'#105b70'
+      }
+    },
   },
   plugins: [],
 }
